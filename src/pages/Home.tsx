45a8d1bc44683f7c2f0e28e62ff8e04e52e87379
aureuneun <aureuneun@aureuneuns-MacBook-Pro.local>
2022-01-20 16:08:27 +0900

    feat: Header

---
+++ src/pages/Home.tsx
@@ -0,0 +1,15 @@
+const Home = () => {
+  return (
+    <div
+      style={{
+        backgroundColor: "whitesmoke",
+        height: "200vh",
+        padding: "100px",
+      }}
+    >
+      Home
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/pages/Tv.tsx
@@ -0,0 +1,15 @@
+const Tv = () => {
+  return (
+    <div
+      style={{
+        backgroundColor: "whitesmoke",
+        height: "200vh",
+        padding: "100px",
+      }}
+    >
+      Tv
+    </div>
+  );
+};
+
+export default Tv;
